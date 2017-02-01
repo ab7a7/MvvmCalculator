18311b59029c73a490e0a3257e34d65805f42eb0
axa21 <axa21@LDPCIT006>
2017-02-01 13:19:05 +0000

    Add project files.

---
+++ Calculator/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return base.CreateShell();
+        }
+    }
+}
